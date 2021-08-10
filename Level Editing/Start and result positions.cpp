0a84c004d28e72697991e39993dd0c242840cf9b
Kell <32032763+kellsnc@users.noreply.github.com>
2021-08-10 14:12:36 +0200

    Update Start and result positions.cpp

--- Level Editing/Start and result positions.cpp
+++ Level Editing/Start and result positions.cpp
@@ -5,29 +5,17 @@ StartPosition level_endpos = { LevelIDs_CityEscape, 0, 0, 0, { 0.0f, 0.0f, 0.0f 
 LevelEndPosition level_2pintro = { LevelIDs_CityEscape, 0, 0, 0, { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f } }; // P1 & P2 start positions & Y rotation in multiplayer
 LevelEndPosition level_endposM23 = { LevelIDs_CityEscape, 0, 0, 0, { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f } }; // Mission 2 & 3 end positions & Y rotation (where sonic does the win stance)
 
-void SetStartEndPoints(const HelperFunctions& helperFunctions, StartPosition* start, LevelEndPosition* start2pIntro, StartPosition* end, LevelEndPosition* missionend) {
-	for (int i = 0; i <= Characters_Amy; i++) {
-		if (start) {
-			helperFunctions.RegisterStartPosition(i, *start);
-		}
+extern "C"
+{
+	__declspec(dllexport) void Init(const char* path, const HelperFunctions& helperFunctions)
+	{
+		helperFunctions.RegisterStartPosition(Characters_Sonic, level_startpos);
+		helperFunctions.Register2PIntroPosition(Characters_Sonic, level_2pintro);
 
-		if (start2pIntro) {
-			helperFunctions.Register2PIntroPosition(i, *start2pIntro);
-		}
-
-		if (helperFunctions.Version >= 5) {
-			if (end) {
-				helperFunctions.RegisterEndPosition(i, *end);
-			}
-
-			if (missionend) {
-				helperFunctions.RegisterMission23EndPosition(i, *missionend);
-			}
+		if (helperFunctions.Version >= 5)
+		{
+			helperFunctions.RegisterEndPosition(Characters_Sonic, level_endpos);
+			helperFunctions.RegisterMission23EndPosition(Characters_Sonic, level_endposM23);
 		}
 	}
 }
-
-extern "C" {
-	__declspec(dllexport) void Init(const char* path, const HelperFunctions& helperFunctions) {
-		SetStartEndPoints(helperFunctions, &level_startpos, &level_2pintro, &level_endpos, &level_endposM23);
-	}
